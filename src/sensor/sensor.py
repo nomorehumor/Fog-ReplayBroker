66cdc03567a6cae82f1dc1e8aeb44952c414c4cc
Maxim Popov <itsmail4max@gmail.com>
2023-07-07 20:04:05 +0200

    updated sensor

--- src/sensor/sensor.py
+++ src/sensor/sensor.py
@@ -8,12 +8,24 @@ import yaml
 import os
 import argparse
 
+DATA_ENERGY_USAGE = "energy_usage"
+DATA_WEATHER = "weather"
+
 def generate_electricity_rows() -> dict:
     while True:
         date = datetime.now()
         ev_usage = round(random.uniform(0, random.uniform(100, 1000)), 2)
         uuid_value = uuid.uuid1()
-        yield {"timestamp": str(date), "uuid": str(uuid_value), "name": "energy_usage", "value": ev_usage}
+        yield {"timestamp": str(date), "uuid": str(uuid_value), "name": DATA_ENERGY_USAGE, "value": ev_usage}
+
+def generate_weather_rows() -> dict:
+    while True:
+        date = datetime.now()
+        temperature = round(random.uniform(-10, 40), 2)
+        humidity = round(random.uniform(0, 100), 2)
+        wind_speed = round(random.uniform(0, 30), 2)
+        uuid_value = uuid.uuid1()
+        yield {"timestamp": str(date), "uuid": str(uuid_value), "name": DATA_WEATHER, "temperature": temperature, "humidity": humidity, "wind_speed": wind_speed}
 
 class Sensor:
 
@@ -23,8 +35,12 @@ class Sensor:
         self.publisher.bind(socket_address)  # Connect socket to backend of the broker
         self.delay = delay
 
-    def start(self):
-        data_generator = generate_electricity_rows()
+    def start(self, data_type):
+        if data_type == DATA_ENERGY_USAGE:
+            data_generator = generate_electricity_rows()
+        elif data_type == DATA_WEATHER:
+            data_generator = generate_weather_rows()
+            
         for data in data_generator:
             self.publish_data(data)
             time.sleep(self.delay)
@@ -38,6 +54,7 @@ class Sensor:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", default=os.path.dirname(os.path.realpath(__file__)) + "/configs/sensor.yaml")
+    parser.add_argument("-t", type=str, required=True, help=f"Options: {DATA_ENERGY_USAGE}, {DATA_WEATHER}")
     args = parser.parse_args()
     
     with open(args.config, "r") as f:
